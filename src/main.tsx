dae240f176be94541d8316392e438a5dba5831a9
Ivan Aguilar <ivanaguiglez@gmail.com>
2023-11-08 00:10:39 +0100

    Add AppStyled

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,19 @@ import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-import "./styles/styles.css";
+import "styled-components";
+import GlobalStyle from "./styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import mainTheme from "./styles/mainTheme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <ThemeProvider theme={mainTheme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
